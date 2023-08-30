22d753173e3ff59d81d16d4b1383cbd9ceaa2f47
sweta <sweta.koski@metropolia.fi>
2023-08-30 10:47:15 +0300

    changes have been made on the line 26

---
+++ main.py
@@ -0,0 +1,26 @@
+"""
+name=input ("Enter your name : ")
+print ("Hello, " + name + "!")
+"""
+"""
+from math import pi
+r= float(input("Enter radius of a circle:"))
+print ("The area of a circle with radius " + str(r) + " is " + str(pi*r**2))
+"""
+"""
+l=int(input("Enter the lenght of a rectangle: "))
+w=int(input("Enter the width of a rectangle: "))
+print ("The area of a rectangle is " + str(l*w)+ ".")
+print("The perimeter of a rectangle is " + str(l*4) +".")
+"""
+
+"""
+
+n1=int(input("Enter the first number: "))
+n2=int(input("Enter your second number: "))
+n3=int(input("Enter your third number: "))
+d=int(n1+n2+n3)/3
+print("The sum is " + str(n1+n2+n3) + ".")
+print("The product is " + str(n1*n2*n3) + ".")
+print ("The average is " + str(d))
+"""
